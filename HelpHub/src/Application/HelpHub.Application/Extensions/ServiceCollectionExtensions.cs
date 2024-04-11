26fef390575e429f47f70b003ba366dd4876b153
Прокопец Семен <112980469+Semen4ikk@users.noreply.github.com>
2024-04-11 12:25:05 +0300

    Add files via upload

--- HelpHub/src/Application/HelpHub.Application/Extensions/ServiceCollectionExtensions.cs
+++ HelpHub/src/Application/HelpHub.Application/Extensions/ServiceCollectionExtensions.cs
@@ -1,3 +1,9 @@
+using HelpHub.Application.Contracts.ServiceInterfaces;
+using HelpHub.Application.Service;
+using HelpHub.Infrastructure.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
 namespace HelpHub.Application.Extensions;
 
 using Microsoft.Extensions.DependencyInjection;
@@ -7,6 +13,18 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddApplication(this IServiceCollection collection)
     {
         // TODO: add services
+        collection.AddScoped<IOrganizatorService, OrganizatorService>();
+        collection.AddScoped<IUserService, UserService>();
+        collection.AddScoped<IEventService, EventService>();
+        collection.AddScoped<IDonateService, DonateService>();
+        collection.AddScoped<ICollaborationService, CollaborationService>();
+        return collection;
+    }
+
+    public static IServiceCollection AddInfrastructurePersistence(this IServiceCollection collection, IConfiguration configuration)
+    {
+        collection.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(configuration.GetSection("Infrastructure:Persistence:Postgres:ConnectionString").Value));
         return collection;
     }
 }
