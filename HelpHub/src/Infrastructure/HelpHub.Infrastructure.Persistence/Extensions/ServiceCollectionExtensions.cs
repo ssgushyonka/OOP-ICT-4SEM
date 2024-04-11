058e174e966a0a0bdbda68f5ad76ee9c0730a522
Прокопец Семен <112980469+Semen4ikk@users.noreply.github.com>
2024-04-11 12:26:05 +0300

    Add files via upload

--- HelpHub/src/Infrastructure/HelpHub.Infrastructure.Persistence/Extensions/ServiceCollectionExtensions.cs
+++ HelpHub/src/Infrastructure/HelpHub.Infrastructure.Persistence/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,9 @@
+using HelpHub.Application.Abstractions.Persistence.Repositories;
+using HelpHub.Infrastructure.Persistence.Repositories;
+using Microsoft.Extensions.Configuration;
+
 namespace HelpHub.Infrastructure.Persistence.Extensions;
 
-using HelpHub.Application.Abstractions.Persistence;
 using HelpHub.Infrastructure.Persistence.Migrations;
 using HelpHub.Infrastructure.Persistence.Plugins;
 using Itmo.Dev.Platform.Postgres.Extensions;
@@ -9,7 +12,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 public static class ServiceCollectionExtensions
 {
-    public static IServiceCollection AddInfrastructurePersistence(this IServiceCollection collection)
+    public static IServiceCollection AddInfrastructurePersistence(
+        this IServiceCollection collection,
+        ConfigurationManager builderConfiguration)
     {
         collection.AddPlatformPostgres(builder => builder.BindConfiguration("Infrastructure:Persistence:Postgres"));
         collection.AddSingleton<IDataSourcePlugin, MappingPlugin>();
@@ -18,8 +23,12 @@ public static class ServiceCollectionExtensions
         collection.AddHostedService<MigrationRunnerService>();
 
         // TODO: add repositories
-        collection.AddScoped<IPersistenceContext, PersistenceContext>();
-
+        // collection.AddScoped<IPersistenceContext, PersistenceContext>();
+        collection.AddScoped<IUserRepository, UserRepository>();
+        collection.AddScoped<IEventRepository, EventRepository>();
+        collection.AddScoped<IDonateRepository, DonateRepository>();
+        collection.AddScoped<IOrganizatorRepository, OrganizatorRepository>();
+        collection.AddScoped<ICollaborationRepository, CollaborationRepository>();
         return collection;
     }
 }
